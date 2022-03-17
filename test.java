ca54f609d2e246c72d1f1f303c81e7a81568d9b5
MissBailey <101813732+MissBailey@users.noreply.github.com>
2022-03-17 09:52:10 -0400

    Add files via upload

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class test
+{
+	public static void main (String[] args) 
+	{
+		int x = 10;
+		for (int i = 0; i < 20; i ++) {
+			System.out.println("X: " + x);
+			System.out.println("i: " + i);
+		}
+		System.out.println("Donzo");
+	}
+}
